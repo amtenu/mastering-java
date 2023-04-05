a075165012f0bf5512315da35add62af1b914b81
amtenu <amannov21@gmail.com>
2023-04-05 11:02:19 -0600

    Adding digits of a given number

---
+++ src/SumDigit.java
@@ -0,0 +1,23 @@
+//Method to add all digits of a number
+//Mod to takeout each digit
+//Divide each remaining digits by 10
+
+
+public class SumDigit {
+    public static void main(String[] args) {
+        System.out.println(addDigits(1));
+    }
+
+    public static int addDigits(int number){
+        if(number<0){
+            return -1;
+        }
+        int sum=0;
+       while (number >0){
+          sum=sum+(number % 10);
+          number =number/10;
+       }
+       return sum;
+    }
+
+}
